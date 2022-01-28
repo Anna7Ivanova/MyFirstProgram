3f4b2cfe803a5a21c9c4ff5d8cf2a6c10da79330
Anna Ivanova <bovbyr@yahoo.co.uk>
2022-01-28 13:33:07 +0200

    Added some console interactions

--- src/main/java/automation/qa/MyFirstJava.java
+++ src/main/java/automation/qa/MyFirstJava.java
@@ -1,2 +1,22 @@
-package automation.qa;public class MyFirstJava {
+package automation.qa;
+
+import java.util.Scanner;
+
+public class MyFirstJava {
+    public static void main(String[] args) {
+        System.out.println("Please enter your first name:");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String firstName = scanner.next();
+
+        System.out.println("Your first name is:" + firstName);
+
+        System.out.println("Please enter your age:");
+
+        int userAge = scanner.nextInt();
+
+        System.out.println("Your age is:" + userAge);
+
+    }
 }
