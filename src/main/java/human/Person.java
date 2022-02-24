141daae6db1425eab48c5cb0f5c5ad9238d544f4
Anna Ivanova <bovbyr@yahoo.co.uk>
2022-02-24 09:05:19 +0200

    Added some console interactions

--- src/main/java/human/Person.java
+++ src/main/java/human/Person.java
@@ -3,25 +3,6 @@ package human;
 import animals.Mammal;
 
 public class Person extends Mammal {
-    @Override
-    public String getName() {
-        return super.getName();
-    }
-
-    @Override
-    public void setName(String name) {
-        super.setName(name);
-    }
-
-    @Override
-    public String getFood() {
-        return super.getFood();
-    }
-
-    @Override
-    public void setFood(String food) {
-        super.setFood(food);
-    }
 
     public Person(String name, String food) {
         super(name, food);
--- src/main/java/human/Test.java
+++ src/main/java/human/Test.java
@@ -2,9 +2,8 @@ package human;
 
 public class Test {
     public static void main(String[] args) {
+
         Person person1 = new Person("Ani", "ice-cream.");
-        //person1.setName("Ani");
-        //person1.setFood("ice-cream");
         person1.eatFood();
 
         System.out.println("--------");
